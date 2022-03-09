023d42446265cdc86ac6876dfab859040fce4b40
Carlos <carloscarucce@gmail.com>
2022-03-09 01:57:12 -0300

    Fix target directory definition

--- src/AppInstaller.php
+++ src/AppInstaller.php
@@ -5,6 +5,7 @@ namespace Vimu\Composer\AppInstallerPlugin;
 use Composer\Installer\InstallerInterface;
 use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
+use InvalidArgumentException;
 
 class AppInstaller extends LibraryInstaller implements InstallerInterface
 {
@@ -14,7 +15,17 @@ class AppInstaller extends LibraryInstaller implements InstallerInterface
      */
     public function getInstallPath(PackageInterface $package)
     {
-        return 'app';
+        $extras = $package->getExtra();
+
+        if (empty($extras['vimu']['app-path'])) {
+            throw new InvalidArgumentException('Missing "extra": vimu > app-path');
+        }
+
+        if (!preg_match('/^[A-Z][a-zA-Z0-9\-_]*$/', $extras['vimu']['app-path'])) {
+            throw new InvalidArgumentException('Invalid "extra" format: vimu>app-path; This MUST not include spacing or special characters.');
+        }
+
+        return 'app/'.$package->getPrettyName();
     }
 
     /**
